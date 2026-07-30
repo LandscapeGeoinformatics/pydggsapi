a0ed0ac0068f99de8e61763644d0d7949c39d6e9
Tik <waiti84@ut.ee>
2026-07-30 12:39:37 +0300

    bugfix zone query: changed to use set to hold filtered zone ids

--- pydggsapi/models/ogc_dggs/zone_query.py
+++ pydggsapi/models/ogc_dggs/zone_query.py
@@ -23,7 +23,7 @@ def query_zones_list(bbox, zone_level, limit, dggrs_info: DggrsDescription, dggr
     logger.debug(f'{__name__} query zones list: {bbox}, {zone_level}, {limit}, {parent_zone}, {compact}')
     # generate zones for the bbox at the required zone_level
     result = dggrs_provider.zoneslist(bbox, zone_level, parent_zone, returngeometry, compact)
-    filter_ = []
+    filter_ = set([])
     cql_attributes = set() if (cql_filter is None) else getCQLAttributes(cql_filter)
     skipped = 0
     for k, v in collection.items():
@@ -65,7 +65,8 @@ def query_zones_list(bbox, zone_level, limit, dggrs_info: DggrsDescription, dggr
         else:
             if (zone_id_repr != 'textual'):
                 filtered_zoneIds = dggrs_provider.zone_id_to_textual(filtered_zoneIds, zone_id_repr, zone_level)
-            filter_ += filtered_zoneIds
+            filter_ = filter_ | set(filtered_zoneIds)
+    filter_ = list(filter_)
     if (skipped == len(collection)):
         raise ValueError(f"{__name__} query zones list cql attributes({cql_attributes}) not found in all collections.")
     if (len(filter_) == 0):
@@ -76,4 +77,4 @@ def query_zones_list(bbox, zone_level, limit, dggrs_info: DggrsDescription, dggr
                     for i, zid in enumerate(result.zones[:limit]) if (zid in filter_)]
         return ZonesGeoJson(**{'type': 'FeatureCollection', 'features': features})
     total_area = sum(np.array(result.returnedAreaMetersSquare)[np.isin(result.zones, filter_)].tolist())
-    return ZonesResponse(**{'zones': np.unique(filter_[:limit]), 'returnedAreaMetersSquare': total_area})
+    return ZonesResponse(**{'zones': filter_[:limit], 'returnedAreaMetersSquare': total_area})
