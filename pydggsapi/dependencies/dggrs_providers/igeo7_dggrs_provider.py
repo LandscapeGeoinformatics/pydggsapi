890f58c5d31f5ebb71716b6b3b657cd81a4778eb
Tik <waiti84@ut.ee>
2026-08-10 11:39:39 +0300

    igeo7 skip authalic conversion if geometry is not required.

--- pydggsapi/dependencies/dggrs_providers/igeo7_dggrs_provider.py
+++ pydggsapi/dependencies/dggrs_providers/igeo7_dggrs_provider.py
@@ -140,48 +140,50 @@ class IGEO7Provider(AbstractDGGRSProvider):
 
         return self.data[zoom]
 
-    def generate_hexgrid(self, bbox, resolution):
+    def generate_hexgrid(self, bbox, resolution, authalic_conversion=True):
         # ISEA7H grid at resolution, for extent of provided WGS84 rectangle into GeoDataFrame
         bbox = _geodetic_to_authalic(bbox, self.wgs84_geodetic_conversion)[0]
         gdf = self.dggrid_instance.grid_cell_polygons_for_extent(self.dggrs, resolution, clip_geom=bbox, **self.properties.__dict__)
-        gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
+        if (authalic_conversion):
+            gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
         return gdf
 
-    def generate_hexcentroid(self, bbox, resolution):
+    def generate_hexcentroid(self, bbox, resolution, authalic_conversion=True):
         # ISEA7H grid at resolution, for extent of provided WGS84 rectangle into GeoDataFrame
         bbox = _geodetic_to_authalic(bbox, self.wgs84_geodetic_conversion)[0]
         gdf = self.dggrid_instance.grid_cell_centroids_for_extent(self.dggrs, resolution, clip_geom=bbox, **self.properties.__dict__)
-        gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
+        if (authalic_conversion):
+            gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
         return gdf
 
     # default values from dggrid4py on clip_subset_type and clip_cell_res
-    def centroid_from_cellid(self, cellid: List[str], zone_level, clip_subset_type='WHOLE_EARTH', clip_cell_res=1):
+    def centroid_from_cellid(self, cellid: List[str], zone_level, clip_subset_type='WHOLE_EARTH', clip_cell_res=1, authalic_conversion=True):
         gdf = self.dggrid_instance.grid_cell_centroids_from_cellids(cellid, self.dggrs, zone_level,
                                                                     clip_subset_type=clip_subset_type,
                                                                     clip_cell_res=clip_cell_res,
                                                                     **self.properties.__dict__)
-        gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
+        if (authalic_conversion):
+            gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
         return gdf
 
     # default values from dggrid4py on clip_subset_type and clip_cell_res
-    def hexagon_from_cellid(self, cellid: List[str], zone_level, clip_subset_type='WHOLE_EARTH', clip_cell_res=1):
+    def hexagon_from_cellid(self, cellid: List[str], zone_level, clip_subset_type='WHOLE_EARTH', clip_cell_res=1, authalic_conversion=True):
         gdf = self.dggrid_instance.grid_cell_polygons_from_cellids(cellid, self.dggrs, zone_level,
                                                                    clip_subset_type=clip_subset_type,
                                                                    clip_cell_res=clip_cell_res,
                                                                    **self.properties.__dict__)
-        gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
+        if (authalic_conversion):
+            gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
         return gdf
 
     def cellid_from_centroid(self, geodf_points_wgs84, zoomlevel):
         geodf_points_wgs84 = _geodetic_to_authalic(geodf_points_wgs84, self.wgs84_geodetic_conversion)
         gdf = self.dggrid_instance.cells_for_geo_points(geodf_points_wgs84, True, self.dggrs, zoomlevel, **self.properties.__dict__)
-        # gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
         return gdf
 
     def cellids_from_extent(self, clip_geom, zoomlevel):
         clip_geom = _geodetic_to_authalic(clip_geom, self.wgs84_geodetic_conversion)[0]
         gdf = self.dggrid_instance.grid_cellids_for_extent(self.dggrs, zoomlevel, clip_geom=clip_geom, **self.properties.__dict__)
-        # gdf.geometry = _authalic_to_geodetic(gdf.geometry, self.wgs84_geodetic_conversion)
         return gdf
 
     def zone_id_from_textual(self, cellIds: List[str], zone_id_repr: str) -> List[Any]:
@@ -267,7 +269,7 @@ class IGEO7Provider(AbstractDGGRSProvider):
 
         if (bbox is not None):
             try:
-                hex_gdf = self.generate_hexcentroid(bbox, zone_level)
+                hex_gdf = self.generate_hexcentroid(bbox, zone_level, authalic_conversion=False)
                 hex_gdf.set_index("name", inplace=True)
             except Exception as e:
                 logger.error(f'{__name__} query zones list, bbox: {bbox} dggrid convert failed :{e}')
@@ -277,7 +279,7 @@ class IGEO7Provider(AbstractDGGRSProvider):
             try:
                 parent_zone_level = self.get_cells_zone_level([parent_zone])[0]
                 childern_hex_gdf = self.centroid_from_cellid([parent_zone], zone_level, clip_subset_type='COARSE_CELLS',
-                                                             clip_cell_res=parent_zone_level)
+                                                             clip_cell_res=parent_zone_level, authalic_conversion=False)
                 childern_hex_gdf.set_index('name', inplace=True)
                 hex_gdf = hex_gdf.join(childern_hex_gdf, how='inner', rsuffix='_p') if (bbox is not None) else childern_hex_gdf
             except Exception as e:
@@ -309,8 +311,9 @@ class IGEO7Provider(AbstractDGGRSProvider):
         geometry = None
         if (returngeometry is not None):
             if (returngeometry != 'zone-centroid'):
-                hex_gdf = self.hexagon_from_cellid(hex_gdf.index.values, zone_level)
+                hex_gdf = self.hexagon_from_cellid(hex_gdf.index.values, zone_level, authalic_conversion=False)
                 hex_gdf.set_index("name", inplace=True)
+            hex_gdf['geometry'] = _authalic_to_geodetic(hex_gdf['geometry'], self.wgs84_geodetic_conversion)
             geotype = GeoJSONPoint if (returngeometry == 'zone-centroid') else GeoJSONPolygon
             geometry = [geotype(**eval(shapely.to_geojson(g))) for g in hex_gdf['geometry'].values.tolist()]
         area = [self.data[zone_level]['Area (km^2)'] * 1000000] * len(hex_gdf)
